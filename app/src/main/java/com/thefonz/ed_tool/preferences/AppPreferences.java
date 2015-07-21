8ffd4b8108c75cab5177bb2be794a6f5ae63a3bd
theFONZ <blackthesoul@gmail.com>
2015-07-21 21:23:53 +0100

    Added ability to reset ALL function buttons
    Added to AppPreferences, option to reset ALL buttons if needed

--- app/src/main/java/com/thefonz/ed_tool/preferences/AppPreferences.java
+++ app/src/main/java/com/thefonz/ed_tool/preferences/AppPreferences.java
@@ -3,6 +3,9 @@ package com.thefonz.ed_tool.preferences;
 /**
  * Created by thefonz on 04/04/15.
  */
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -19,7 +22,7 @@ public class AppPreferences extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
 
-        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        final SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 //        boolean immersiveMode = SP.getBoolean("immersiveMode",false);
         String selectTheme = SP.getString("selectTheme", "1");
 
@@ -40,8 +43,33 @@ public class AppPreferences extends PreferenceActivity {
         Preference myPref = findPreference("saveGoBack");
         myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-                //open browser or intent here
                 navigateUpTo(new Intent(getApplicationContext(), ED_Tool.class));
+                return true;
+            }
+        });
+
+        // set resetAllPrefs preference onclick
+        Preference myPrefReset = findPreference("resetAllPrefs");
+        myPrefReset.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AppPreferences.this);
+                builder.setTitle("Reset ALL Button's back to default ? ");
+                builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // reset all button preferences...
+                        SharedPreferences myPrefs = getApplicationContext().getSharedPreferences("myPrefs", MODE_WORLD_READABLE);
+                        myPrefs.edit().clear().commit();
+                        navigateUpTo(new Intent(getApplicationContext(), ED_Tool.class));
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                       dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
                 return true;
             }
         });
@@ -51,5 +79,4 @@ public class AppPreferences extends PreferenceActivity {
     public void onBackPressed() {
         // Leave blank if you do not want anything to happen
     }
-
 }
